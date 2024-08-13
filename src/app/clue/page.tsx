66737d6336b9f3101f73f87827ed7144f5902ba5
jetta <jettaphoom@gmail.com>
2024-08-13 14:37:13 +0700

    backup

---
+++ src/app/clue/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '@/styles/clue.module.css'
+const Clue = () => {
+  return (
+    <main className={styles.background}>
+      <h1>Clue</h1>
+    </main>
+  )
+}
+
+export default Clue
